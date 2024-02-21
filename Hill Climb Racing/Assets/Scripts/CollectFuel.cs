51b651625ce428ceeaafae27e3171ba3b3eaf2c8
MarioCastilloArenas <127202707+MarioCastilloArenas@users.noreply.github.com>
2024-02-21 13:36:02 +0100

    invisible wall added

--- Hill Climb Racing/Assets/Scripts/CollectFuel.cs
+++ Hill Climb Racing/Assets/Scripts/CollectFuel.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectFuel : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    
+    private void OnTriggerEnter2D(Collider2D col)
 
     {
         // we give to the whole car the tag 'player' so when it touches the can of gas the methos fillfuel is called.
-        if(collision.gameObject.CompareTag("Player"))
+        if(col.gameObject.CompareTag("Player"))
         {
             FuelController.instance.FillFuel();
             Destroy(gameObject);
         }
     }
+
+    
 }
